c9ded43d88278f4b46aa73002d29ccb402142e39
Edwin García <eagp-efv33@proton.me>
2024-11-08 19:31:00 -0500

    This is my firts commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/*Se cre una variable constante con nombre "http" que cuarda una libreria para node js y su conexión con el front*/
+
+const http = require("http");
+
+/* Una función es un pedazo de codigo que nos permite realizar alguna funcionalidad 
+    dependiendo del requerimiento del desarrollo de la aplicación, en este caso se crea 
+    una function para controlar las peticiones que llegan a esta maquina*/
+
+function requestController(){
+     console.log({http});
+}
+
+const server = http.createServer(requestController);    
+
+server.listen(4500);
